fa28601ade6da79e3475a9a6b65825f64061d3ce
Jordan Pritt <pritt.jordan@gmail.com>
2020-05-01 21:32:35 -0400

    Added the Capstone Project to repo.

---
+++ TeacherAssistant/TeacherAssistant/Data/TeacherAssistantContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeacherAssistant.Models;
+
+namespace TeacherAssistant.Data
+{
+    public class TeacherAssistantContext : DbContext
+    {
+        public TeacherAssistantContext (DbContextOptions<TeacherAssistantContext> options) : base(options)
+        { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ TeacherAssistant/TeacherAssistant/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeacherAssistant.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Grade { get; set; }
+
+        [Display(Name = "Teacher ID")]
+        [Required]
+        public int TeacherId { get; set; }
+    }
+}
