25e80d639ec0c41902a58d1f4f7c4b451307b975
Jordan Pritt <pritt.jordan@gmail.com>
2020-05-21 00:51:41 -0400

    Added test project.

---
+++ TeacherAssistant/TeacherAssistant.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TeacherAssistant.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
